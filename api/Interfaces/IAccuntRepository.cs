bf77cecf8142916491186c714ad24e62522f633b
Reyhaneh-1386 <r9361250@gmail.com>
2025-04-25 00:38:52 +0330

    Interface

--- api/Interfaces/IAccuntRepository.cs
+++ api/Interfaces/IAccuntRepository.cs
@@ -12,11 +12,7 @@ public interface IAccuntRepository
 {
     public Task<LoggedInDto?> RegisterAsync(AppUser UserInput, CancellationToken cancellationToken);
     public Task<LoggedInDto?> LoginAsynce(AppUser UserInput, CancellationToken cancellationToken);
-    public Task<List<AppUser>?> GetAllSynce (CancellationToken cancellationToken);
-    public Task<LoggedInDto?> UpdateByIdAsynce (string userid, AppUser userInput,CancellationToken cancellationToken);
-    public Task<DeleteResult?> DeleteByIdAsynce (string userId, CancellationToken cancellationToken);
-
-
-
-
+    public Task<List<AppUser>?> GetAllSynce(CancellationToken cancellationToken);
+    public Task<LoggedInDto?> UpdateByIdAsynce(string userid, AppUser userInput, CancellationToken cancellationToken);
+    public Task<DeleteResult?> DeleteByIdAsynce(string userId, CancellationToken cancellationToken);
 }
