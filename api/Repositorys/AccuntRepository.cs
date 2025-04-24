86f16aedccbb978073720a5bf0b5136d76d5e2ff
Reyhaneh-1386 <r9361250@gmail.com>
2025-04-25 00:44:28 +0330

    AccuntRepository

--- api/Repositorys/AccuntRepository.cs
+++ api/Repositorys/AccuntRepository.cs
@@ -2,10 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.DTOs;
+using api.Interfaces;
+using api.Models;
+using api.Settings;
+using MongoDB.Driver;
 
 namespace api.Repositorys;
 
-public class AccuntRepository
+public class AccuntRepository : IAccuntRepository
 {
-    
+ private readonly IMongoCollection<AppUser> _collection;
+
+    // Dependency Injection
+    public AccuntRepository(IMongoClient client, IMongoDbSettings dbSettings)
+    {
+        var dbName = client.GetDatabase(dbSettings.DatabaseName);
+        _collection = dbName.GetCollection<AppUser>("users");
+    }
+
+    public Task<DeleteResult?> DeleteByIdAsynce(string userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<AppUser>?> GetAllSynce(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoggedInDto?> LoginAsynce(AppUser UserInput, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoggedInDto?> RegisterAsync(AppUser UserInput, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoggedInDto?> UpdateByIdAsynce(string userid, AppUser userInput, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
