c43cfb8af5380dcce5e63a602924bf661e7aea82
Reyhaneh-1386 <r9361250@gmail.com>
2025-04-25 15:42:48 +0330

    client

---
+++ client/src/app/models/app-user.moder.ts
@@ -0,0 +1,14 @@
+export interface AppUser {
+    email: string;
+    name: string;
+    lastName: string;
+    nationalId: string;
+    password: string;
+    confirmPassword: string;
+    gender: string;
+    country: string;
+    city: string;
+    skillLevel: string;
+    age: number;
+    datOfBirth: number;
+}
---
+++ client/src/app/models/logged-in.model.ts
@@ -0,0 +1,4 @@
+export interface LoggedIn {
+    email: string;
+    Name: string;
+}
