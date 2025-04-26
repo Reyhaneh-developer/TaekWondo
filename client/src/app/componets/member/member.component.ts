265dcc0bf3496e14498a6028c413174016428fdf
Reyhaneh-1386 <r9361250@gmail.com>
2025-04-26 15:07:25 +0330

    meber

--- client/src/app/componets/member/member.component.ts
+++ client/src/app/componets/member/member.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { RouterLink } from '@angular/router';
+import { Member } from '../../models/member.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-member',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss'
 })
 export class MemberComponent {
+  accessorService = inject(AccountService);
+  membrs: Member[] | undefined;
 
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(): void {
+    this.accessorService.getAllMember().subscribe({
+      next: (res) => {
+        this.membrs = res
+      }
+    })
+  }
 }
